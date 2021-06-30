03100c03757b7595240c3ea5e5d81e4d94915a72
William <williancard123@gmail.com>
2021-06-30 12:08:13 -0400

    teste

--- sendServer.py
+++ sendServer.py
@@ -1,31 +1,45 @@
+"""
+- the server will send the files and folders
+"""
+
 import os
 import socket
 import threading
 
 print('[+]- Server')
+
 # server
 
 bind_ip = '0.0.0.0'
 bind_port = 5043
 
 
+def check_log_exists():
+    namelogfile = 'logshipping.log'
+    pathDesktop = f"{os.path.join(os.environ['USERPROFILE'], 'desktop')}"
+    pathLogfile = os.path.join(pathDesktop, namelogfile) # log file path in the desktop
+
+    if os.path.isfile(pathLogfile):
+        print('existe')
+    else:
+        os.mkdir(pathLogfile)
+
+check_log_exists()
+
 def server():
     instancesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     instancesocket.bind((bind_ip, bind_port))
     instancesocket.listen(10)
-
     print(f'the server is open at the port: {bind_port}')
 
     def treatCustomer(socketClient):
         #  receive what the customer sends
-        socketClient.sendall(b'HTTP/1.0 200 OK\r\nContent-Length: 11\r\nContent-Type: text/html; charset=UTF-8\r\n\r\nHello World\r\n')
-        data = socketClient.recv(8192)
-        print(f'received {data} ')
         socketClient.close()
 
     while True:
-        socketclient , addr = instancesocket.accept()
-        print(f'Conectado a {addr}')
+        socketclient, addr = instancesocket.accept()
+        print(f'connectend there is: {addr}')
+
         clienthread = threading.Thread(target=treatCustomer, args=(socketclient, ))
         clienthread.start()
 
